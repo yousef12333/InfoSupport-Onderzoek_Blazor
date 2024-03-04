8a691f9d8c6221bddfd5e13dd3abad68032a5f27
Youssef Boukhada <youssef.boukhada@infosupport.com>
2024-03-04 10:33:11 +0100

    Make functionality work with logging

--- Blazor_Project/Program.cs
+++ Blazor_Project/Program.cs
@@ -1,11 +1,15 @@
 using Blazor_Project;
+using Blazor_Project.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddScoped<PasswordStrengthService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
+
 await builder.Build().RunAsync();
+
